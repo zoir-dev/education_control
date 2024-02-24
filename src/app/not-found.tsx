929e3d843f9137ab79bc259c918bc2842f53eb54
Zoirjon <zoirjonabduvohidov0@gmail.com>
2024-02-24 17:02:34 +0500

    finished errors

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,13 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 const NotFound = () => {
     const router = useRouter()
-    router.push('/')
+    useEffect(() => {
+        router.push('/')
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 }
 
 export default NotFound
